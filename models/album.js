59edd1d0851072c9a911ae54ff96995708b4a7b4
Eva <evaliisagalen@gmail.com>
2017-12-15 11:33:12 -0800

    added song.js and created references to other files

--- models/album.js
+++ models/album.js
@@ -2,13 +2,15 @@
 // set up shorthand Schema variable to stand in for mongoose.Schema
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Song = require('./song');
 
 // album schema
 var AlbumSchema = new Schema({
   artistName: String,
   name: String,
   releaseDate: String,
-  genres: [ String ]
+  genres: [ String ],
+  song: [Song.schema]
 });
 
 // album model
---
+++ models/song.js
@@ -0,0 +1,15 @@
+// require mongoose
+// set up shorthand Schema variable to stand in for mongoose.Schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// album schema
+var SongSchema = new Schema({
+  name: String,
+  trackName: Number
+});
+
+// album model
+var Song = mongoose.model('Song', SongSchema);
+
+module.exports = Song;
